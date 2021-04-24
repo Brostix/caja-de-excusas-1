c3992f46847cfda1cfedd1947acbbf6fa2f07594
Mario Sorrentino <eclest@gmail.com>
2021-04-24 23:02:10 +0000

    console log de excusas

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 
-let who = ['The dog','My grandma','His turtle','My bird']; 
-let action = ['ate','peed','crushed','broke'];
-let what = ['my homework', 'the keys', 'the car'];
-let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
+function linea1(){
+        let who = ['The dog','My grandma','His turtle','My bird']; 
+        let action = ['ate','peed','crushed','broke'];
+        let what = ['my homework', 'the keys', 'the car'];
+        let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
 
-
-function linea1(num1, num2, num3, num4){
-
-    return num1[Math.floor(Math.random()*who.length)]+ " " + num2[Math.floor(Math.random()*action.length)]+ " " +num3[Math.floor(Math.random()*what.length)]+ " " +num4[Math.floor(Math.random()*when.length)];
+    return who[Math.floor(Math.random()*who.length)]+ " " + action[Math.floor(Math.random()*action.length)]+ " " +what[Math.floor(Math.random()*what.length)]+ " " +when[Math.floor(Math.random()*when.length)];
 }
-console.log(linea1(who,action,what,when))
+console.log(linea1())
+
